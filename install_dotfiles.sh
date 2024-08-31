69a4b2745cdce250bb1aa20dad65697696f95bcb
sky <ryanmcvicker@protonmail.com>
2024-08-31 18:25:39 -0400

    updated install_dotfiles.sh script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,28 +3,22 @@
 
 if [ $SHELL == /bin/zsh ]; then
 
-    if [ -a $HOME/.zshenv ]; then
-        echo ".zshrc is already present "
-    else;
+    if ! [ -a $HOME/.zshenv ]; then
         cp ../dotfiles/aliases $HOME/.zshenv
     fi
 
-elif [ $SHELL == /bin/bash]; then
-    if [ -a $HOME/.aliases ]; then
-        echo ".zshrc is already present "
-    else;
+else
+    if ! [ -a $HOME/.aliases ]; then
         cp ../dotfiles/aliases $HOME/.aliases
     fi
-
-
 fi
 
 
-cat ../dotfiles/zshrc > ~/.zshrc 
-cat ../dotfiles/bashrc > ~/.bashrc 
-cat ../dotfiles/tmux.conf > ~/.tmux.conf
-cat ../dotfiles/vimrc > ~/.vimrc
-cat ../dotfiles/xinitrc > ~/.xinitrc
+cat zshrc > ~/.zshrc 
+cat bashrc > ~/.bashrc 
+cat tmux.conf > ~/.tmux.conf
+cat vimrc > ~/.vimrc
+cat xinitrc > ~/.xinitrc
 
 
 
